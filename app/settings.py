a1497de63be320cd5e6f714e8c2eb8a1d584d5fa
ig-rudenko <ig.rudenko1@yandex.ru>
2024-04-22 13:52:26 +0300

    v 0.2.0 Добавлен celery для создания превью книги Bugfix: docker-compose.yaml, media storage

--- app/settings.py
+++ app/settings.py
@@ -5,7 +5,8 @@ from pydantic_settings import BaseSettings
 
 class _BaseSettings(BaseSettings):
     # Путь к медиа хранилищу
-    media_root: Path = Path(__file__).parent.parent / "media"
+    media_storage: str = "./media"
+    media_root: Path = Path(media_storage)
     media_root.mkdir(exist_ok=True, parents=True)
 
     # Путь к базе данных
