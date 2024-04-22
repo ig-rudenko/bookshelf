2ab92762adebae9692e1811c6746871fd9e4039c
ig-rudenko <ig.rudenko1@yandex.ru>
2024-04-22 15:37:48 +0300

    v 0.2.2 Bugfix thumbnail

--- app/services/thumbnail.py
+++ app/services/thumbnail.py
@@ -15,7 +15,7 @@ def get_thumbnail(image: str, size: Literal["small", "medium"]) -> str:
 
 def create_thumbnails(image_path: Path) -> None:
     """Create a thumbnails of the given image."""
-    img = Image.open(image_path.absolute().as_posix())
     for size_name, size in thumbnail_sizes.items():
+        img = Image.open(image_path.absolute().as_posix())
         img.thumbnail(size)
         img.save(image_path.parent / image_path.name.replace(".png", f"_thumb_{size_name}.png"))
