40800d2dc1917bf091e5695ee466b5b541014161
ig-rudenko <ig.rudenko1@yandex.ru>
2024-04-24 17:45:08 +0300

    v 0.2.10 Добавлен просмотр pdf документа и возможностью поставить закладку для чтения

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -2,3 +2,4 @@
 declare module 'primevue/inlinemessage/InlineMessage.vue';
 declare module 'primevue/autocomplete/AutoComplete.vue';
 declare module '@/components/Menu.vue';
+declare module '@/pages/ShowBook.vue';
