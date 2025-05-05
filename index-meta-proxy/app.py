1dcb0515469cb280240a969bf494503f9868ffb5
ig-rudenko <ig.rudenko1@yandex.ru>
2025-05-06 00:49:57 +0300

    Bugfix отображения приватных книг.

--- index-meta-proxy/app.py
+++ index-meta-proxy/app.py
@@ -118,9 +118,13 @@ async def to_async_gen(data: str) -> AsyncIterable[bytes]:
 async def index(path: str):
     if match := re.match(r"/book/(\d+)", path):
         book_id = int(match.group(1))
-        new_data = await replace_meta_data(book_id)
 
-        return StreamingResponse(to_async_gen(new_data), media_type="text/html")
+        try:
+            new_data = await replace_meta_data(book_id)
+        except Exception as exc:
+            print(exc)
+        else:
+            return StreamingResponse(to_async_gen(new_data), media_type="text/html")
 
     origin_index_data = await get_origin_index()
     return StreamingResponse(to_async_gen(origin_index_data), media_type="text/html")
